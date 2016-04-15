5ff7ebf843a6f3294c0dc949af5033daa7873cf9
manager <manager@gmail.com>
2016-04-15 10:34:42 +0800

    create Class Dog

---
+++ helloWorld/helloWorld/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  helloWorld
+//
+//  Created by zxj on 16/4/15.
+//  Copyright © 2016年 zxj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ helloWorld/helloWorld/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  helloWorld
+//
+//  Created by zxj on 16/4/15.
+//  Copyright © 2016年 zxj. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
